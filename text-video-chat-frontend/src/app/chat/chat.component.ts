101f263533209774d30553ec7e4bd1138cbaf595
EthanBolton <ejbolts@hotmail.com>
2023-09-08 10:58:17 +1000

    added chat component html, log out, leave group, updated styles

--- text-video-chat-frontend/src/app/chat/chat.component.ts
+++ text-video-chat-frontend/src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Group } from '../models/group.model';
 import { User } from '../models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
@@ -13,7 +14,7 @@ export class ChatComponent implements OnInit {
   allGroups: Group[] = [];
   currentUser: User | null = null;
   users: User[] = [];
-
+  constructor(private router: Router) { } // Inject the Router
   // Update your ngOnInit method:
   
   ngOnInit(): void {
@@ -62,5 +63,61 @@ this.currentUser = user; // Important to update the component's state.
     const pending = this.currentUser?.pendingGroups?.includes(groupId) || false;
     return pending;
  }
- 
+
+ logout(): void {
+  sessionStorage.removeItem('currentUser'); // Assuming 'currentUser' is the session key
+  this.router.navigate(['/login']); 
+}
+isMemberOfGroup(groupId: string): boolean {
+  // Find the group with the given groupId
+  const group = this.allGroups.find(g => g.id === groupId);
+
+  // If the group exists and the current user's ID is part of its users, return true
+  const isMember = group?.users?.includes(this.currentUser?.id || '') || false;
+  console.log(`Is user member of group ${groupId}? ${isMember}`);
+  return isMember;
+}
+
+
+leaveGroup(groupId: string): void {
+  const users: User[] = JSON.parse(localStorage.getItem('users') || '[]');
+  const groups: Group[] = JSON.parse(localStorage.getItem('groups') || '[]');
+
+  // Remove groupId from the user's groups
+  if (this.currentUser?.groups) {
+      const index = this.currentUser.groups.indexOf(groupId);
+      if (index > -1) {
+          this.currentUser.groups.splice(index, 1);
+      }
+
+      // Update the user in local storage
+      const userIndex = users.findIndex(u => u.id === this.currentUser?.id);
+      if (userIndex > -1) {
+          users[userIndex] = this.currentUser;
+      }
+  }
+
+  // Remove userId from the group's users
+  const group = groups.find(g => g.id === groupId);
+  if (group?.users) {
+      const index = group.users.indexOf(this.currentUser?.id || '');
+      if (index > -1) {
+          group.users.splice(index, 1);
+      }
+  }
+
+  // Update the groups in local storage
+  localStorage.setItem('users', JSON.stringify(users));
+  localStorage.setItem('groups', JSON.stringify(groups));
+
+  // Refresh the allGroups and currentUser from local storage to update UI
+  this.allGroups = JSON.parse(localStorage.getItem('groups') || '[]');
+  const storedUser = sessionStorage.getItem('currentUser');
+  if (storedUser) {
+      this.currentUser = JSON.parse(storedUser);
+  }
+
+  alert('You have left the group.');
+}
+
 }
