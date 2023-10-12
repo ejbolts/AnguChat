e8c07ce5285ba7de82bdf882254a95c618bddfd7
EthanBolton <ejbolts@hotmail.com>
2023-10-12 10:57:09 +1000

    working socket and viewing of groups and channel in UI

--- text-video-chat-frontend/src/app/chat/chat.component.ts
+++ text-video-chat-frontend/src/app/chat/chat.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { Channel } from '../models/channel.model';
 import { UserService } from '../services/user.service';
 import { ChatService } from '../services/chat.service' ; 
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -15,6 +16,7 @@ import { ChatService } from '../services/chat.service' ;
 export class ChatComponent implements OnInit {
   public messages: string[] = [];
   public message: string = '';
+  groupChannels: { [groupId: string]: Channel[] } = {};  // Map to hold channels for each group
   allGroups: Group[] = [];
   currentUser: User | null = null;
   users: User[] = [];
@@ -22,7 +24,7 @@ export class ChatComponent implements OnInit {
 
   
   ngOnInit(): void {
-    this.allGroups = JSON.parse(localStorage.getItem('groups') || '[]');
+    this.fetchGroups();
     const storedUser = sessionStorage.getItem('currentUser');
     if (storedUser) {
         this.currentUser = JSON.parse(storedUser);
@@ -31,6 +33,35 @@ export class ChatComponent implements OnInit {
     this.chatService.getMessages().subscribe((msg: string) => {
       this.messages.push(msg);
     });
+  }
+  fetchGroups(): void {
+    this.userService.fetchAllGroups().subscribe(
+      (data: Group[]) => {
+        this.allGroups = data;
+  
+        // For each group fetched, fetch its channels
+        this.allGroups.forEach(group => {
+          this.fetchChannelsPerGroup(group._id);
+          
+        });
+  
+      },
+      (error: any) => {
+        console.error("Error fetching groups:", error);
+      }
+    );
+  }
+  fetchChannelsPerGroup(groupId: string): void {
+    console.log(`Fetching channels for group ${groupId}`); 
+
+    this.userService.getChannelsByGroupId(groupId).subscribe(
+      (channels: Channel[]) => {
+        this.groupChannels[groupId] = channels;
+      },
+      error => {
+        console.error("Error fetching channels:", error);
+      }
+    );
   }
   sendMessage() {
     this.chatService.sendMessage(this.message);
@@ -61,57 +92,44 @@ export class ChatComponent implements OnInit {
   sessionStorage.removeItem('currentUser'); // Assuming 'currentUser' is the session key
   this.router.navigate(['/login']); 
 }
+
 isMemberOfGroup(groupId: string): boolean {
-  // Find the group with the given groupId
   const group = this.allGroups.find(g => g._id === groupId);
+  
+  if (!group) {
+    console.error('Group not found:', groupId);
+    return false;
+  }
 
-  // If the group exists and the current user's ID is part of its users, return true
-  const isMember = group?.users?.includes(this.currentUser?._id || '') || false;
-  console.log(`Is user member of group ${groupId}? ${isMember}`);
-  return isMember;
+  return group.users?.includes(this.currentUser?._id ?? '') || false;
 }
 
 
-leaveGroup(groupId: string): void {
-  const users: User[] = JSON.parse(localStorage.getItem('users') || '[]');
-  const groups: Group[] = JSON.parse(localStorage.getItem('groups') || '[]');
+removeUserFromGroup(groupId: string): void {
+  const currentUserId = JSON.parse(sessionStorage.getItem('currentUser')!)?._id ?? '';
+  this.userService.removeUserFromGroup(groupId, currentUserId).subscribe(
+    () => {
+      console.log('User removed from group successfully');
+      this.fetchGroups();  // to refresh the group data and reflect changes
 
-  // Remove groupId from the user's groups
-  if (this.currentUser?.groups) {
-      const index = this.currentUser.groups.indexOf(groupId);
-      if (index > -1) {
-          this.currentUser.groups.splice(index, 1);
-      }
-
-      // Update the user in local storage
-      const userIndex = users.findIndex(u => u._id === this.currentUser?._id);
-      if (userIndex > -1) {
-          users[userIndex] = this.currentUser;
-      }
-  }
-
-  // Remove userId from the group's users
-  const group = groups.find(g => g._id === groupId);
-  if (group?.users) {
-      const index = group.users.indexOf(this.currentUser?._id || '');
-      if (index > -1) {
-          group.users.splice(index, 1);
-      }
-  }
-
-  // Update the groups in local storage
-  localStorage.setItem('users', JSON.stringify(users));
-  localStorage.setItem('groups', JSON.stringify(groups));
-
-  // Refresh the allGroups and currentUser from local storage to update UI
-  this.allGroups = JSON.parse(localStorage.getItem('groups') || '[]');
-  const storedUser = sessionStorage.getItem('currentUser');
-  if (storedUser) {
-      this.currentUser = JSON.parse(storedUser);
-  }
-
-  alert('You have left the group.');
+    },
+    (error: any) => {
+      console.error('Error removing user from group:', error);
+    }
+  );
 }
+addUserToChannel(channelId: string, groupId: string, userId: string): void {
+  this.userService.addUserToChannel(channelId, groupId, userId).subscribe(
+    () => {
+      console.log('User added to channel successfully');
+      this.fetchChannelsPerGroup(groupId);
+    },
+    (error: any) => {
+      console.error('Error adding user to channel:', error);
+    }
+  );
+}
+
 
 deleteAccount(user: User): void {
     // Confirmation before deleting
@@ -129,43 +147,26 @@ deleteAccount(user: User): void {
 
 }
 
-joinChannel(channelId: string, groupId: string): void {
 
-  if (!this.currentUser) {
-    console.error('No current user found!');
-    return;
-  }
-  const group: Group | undefined = this.allGroups.find(g => g._id === groupId);
-  console.log('group', group);
-  console.log('channelId', channelId);
-  console.log('groupId', groupId);
-  if (!group) {
-    console.error('No matching group found!');
-    return;
-  }
-  const channel: Channel | undefined = group.channels.find(ch => ch._id === channelId);
-  if (!channel) {
-    console.error('No matching channel found!');
-    return;
-  }
-  // Ensure users array exists
-  if (!channel.users) {
-    channel.users = [];
-  }
-  // Add the user to the channel's users if not already there
-  if (!channel.users.includes(this.currentUser._id || '')) {
-    channel.users.push(this.currentUser._id || '');
-    // Update groups in your storage
-    this.updateGroupsStorage();
-  }
-}
 
 isChannelMember(channelId: string, groupId: string): boolean {
-  const group: Group | undefined = this.allGroups.find(g => g._id === groupId);
-  const channel: Channel | undefined = group?.channels.find(ch => ch._id === channelId);
-  return channel?.users?.includes(this.currentUser?._id || '') || false;
+  // Check if the channels for the group are fetched and stored
+  if (!this.groupChannels[groupId]) {
+    console.error('Channels for group not yet fetched or not found:', groupId);
+    return false;
+  }
+
+  // Find the channel within the groupChannels map
+  const channel = this.groupChannels[groupId].find(c => c._id === channelId);
+
+  if (!channel) {
+    console.error('Channel not found:', channelId);
+    return false;
+  }
+
+  return channel.users?.includes(this.currentUser?._id ?? '') || false;
 }
-// chat.component.ts
+
 get isAdmin(): boolean {
   const currentUser = JSON.parse(sessionStorage.getItem('currentUser') || '{}');
   return currentUser.role === 'groupAdmin' || currentUser.role === 'superAdmin';
