8c6c8b0bf00058829cfb5f9c4d9457573599e0fb
EthanBolton <ejbolts@hotmail.com>
2024-11-11 00:48:11 +1000

    added update username api

--- server/routes/update.js
+++ server/routes/update.js
@@ -25,4 +25,26 @@ router.put("/:id/role", async (req, res) => {
   }
 });
 
+router.put("/:id/username", async (req, res) => {
+  await connect();
+  const userId = new ObjectId(req.params.id);
+  const { username } = req.body;
+
+  if (!username) {
+    return res.status(400).json({ message: "username is required." });
+  }
+
+  try {
+    await db()
+      .collection("users")
+      .findOneAndUpdate({ _id: userId }, { $set: { username: username } });
+    res.json({ message: "Username updated!", username: username });
+  } catch (err) {
+    console.error("Error updating username:", err);
+    res.status(500).json({ message: "Error updating username." });
+  } finally {
+    close();
+  }
+});
+
 module.exports = router;
